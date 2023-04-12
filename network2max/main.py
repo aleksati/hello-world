e797fd341874351197abc6072de54f192a5a489e
Aleksander Tidemann <aleksandertid@gmail.com>
2023-04-12 18:03:31 +0200

    Made sure "IP" in included in packet + added local_ip to parser

--- network2max/main.py
+++ network2max/main.py
@@ -10,20 +10,21 @@ from pythonosc import udp_client
 
 # cap = pyshark.LiveCapture(interface="Wi-Fi")
 
-host_ip = "192.168.0.118"
-max_pkt_len = ""
+host_ip = "192.168.86.39"
 prev_pkt_len = ""
+# max_pkt_len = ""
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--oscip", default="127.0.0.1")
-    parser.add_argument("--oscport", type=int, default=8888)
+    parser.add_argument("--osc_ip", default="127.0.0.1")
+    parser.add_argument("--local_ip", default=host_ip)
+    parser.add_argument("--osc_port", type=int, default=8888)
     args = parser.parse_args()
 
-    client = udp_client.SimpleUDPClient(args.oscip, args.oscport)
+    client = udp_client.SimpleUDPClient(args.osc_ip, args.osc_port)
 
-    print(f"Sending OSC packets to {args.oscip} on port {args.oscport}.")
+    print(f"Sending OSC packets to {args.osc_ip} on port {args.osc_port}.")
     print(f"Press Ctrl+C to stop.")
 
     # with geoip2.database.Reader('/path/to/maxmind-database.mmdb') as reader:
@@ -33,33 +34,35 @@ if __name__ == "__main__":
     for pkt in cap:
         # Prevent identical connections from happening twice
         if (prev_pkt_len != pkt.length):
-            protocol = pkt.highest_layer
-            source = pkt.ip.src
-            dest = pkt.ip.dst
-            prev_pkt_len = pkt.length
 
-            try:
-                print("[Protocol:] "+protocol+" [Source IP:] "+source +
-                      " [Destination IP:] "+dest+" [Size:] "+pkt.length)
+            if "IP" in pkt:
+                protocol = pkt.highest_layer
+                source = pkt.ip.src
+                dest = pkt.ip.dst
+                prev_pkt_len = pkt.length
 
-                # To OSC
-                client.send_message("/packet_len", int(pkt.length))
+                try:
+                    print("[Protocol:] "+protocol+" [Source IP:] "+source +
+                        " [Destination IP:] "+dest+" [Size:] "+pkt.length)
 
-                if pkt.highest_layer == "TLS":
-                    client.send_message("/protocol", 0)
-                elif pkt.highest_layer == "TCP":
-                    client.send_message("/protocol", 1)
-                elif pkt.highest_layer == "DNS":
-                    client.send_message("/protocol", 2)
-                elif pkt.highest_layer == "HTML":
-                    client.send_message("/protocol", 3)
-                else:
-                    client.send_message("/protocol", 4)
+                    # To OSC
+                    client.send_message("/packet_len", int(pkt.length))
 
-                if source == host_ip:
-                    #dsp = reader.city(pkt.ip.src)
-                    client.send_message('/src_ip', source)
-                    client.send_message("/dst_ip", dest)
+                    if pkt.highest_layer == "TLS":
+                        client.send_message("/protocol", 0)
+                    elif pkt.highest_layer == "TCP":
+                        client.send_message("/protocol", 1)
+                    elif pkt.highest_layer == "DNS":
+                        client.send_message("/protocol", 2)
+                    elif pkt.highest_layer == "HTML":
+                        client.send_message("/protocol", 3)
+                    else:
+                        client.send_message("/protocol", 4)
 
-            except:
-                print("Error with sending OSC..")
+                    if source == args.local_ip:
+                        #dsp = reader.city(pkt.ip.src)
+                        client.send_message('/src_ip', source)
+                        client.send_message("/dst_ip", dest)
+
+                except:
+                    print("Error with sending OSC..")
--- network2max/test.py
+++ network2max/test.py
@@ -2,6 +2,6 @@ import pyshark
 # import sys
 # print(sys.stdout.encoding)
 
-cap = pyshark.LiveCapture(interface='wifi')
-cap.set_debug()
-cap.sniff(timeout=1)
+cap = pyshark.LiveCapture(interface='Wi-Fi')
+cap.sniff(timeout=10)
+print(cap[0])
